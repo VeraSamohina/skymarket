5bdac6be8011116b61c84e77ad641deed34af62c
samohianvera <samohinavera@gmail.com>
2024-01-02 15:22:00 +0300

    fix comments viewset and urls

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,18 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
 from ads.apps import SalesConfig
 from ads.views import AdViewSet, CommentViewSet, MyAdListAPIView
 
 ads_router = SimpleRouter()
-ads_router.register("ads", AdViewSet)
+ads_router.register("ads", AdViewSet, basename='ads')
 
-comment_router = SimpleRouter()
+comment_router = NestedSimpleRouter(ads_router, 'ads', lookup='ad')
 comment_router.register("comments", CommentViewSet, basename='comments')
 app_name = SalesConfig.name
 
 urlpatterns = [
     path("", include(ads_router.urls)),
     path("ad/me/", MyAdListAPIView.as_view(), name='user_ads'),
-    path("comments/", include(comment_router.urls)),
+    path("", include(comment_router.urls)),
 ]
--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -13,11 +13,15 @@ class AdPagination(pagination.PageNumberPagination):
 
 
 class AdViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели Объявление (Ad).
+    Используется созданный фильтр по названию и пагинация.
+    При создании поле "автор" заполняется текущим пользователем
+    """
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AdFilter
     pagination_class = AdPagination
     queryset = Ad.objects.all()
-
 
     def get_serializer_class(self):
         """
@@ -36,19 +40,36 @@ class AdViewSet(viewsets.ModelViewSet):
 
 
 class MyAdListAPIView(generics.ListAPIView):
+    """
+    Класс представление для вывода списка объявлений текущего пользователя
+    """
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
     pagination_class = AdPagination
 
     def list(self, request, *args, **kwargs):
         """
-        Список всех объявлений выбранного пользователя.
+        Список всех объявлений текущего пользователя.
         """
         self.queryset = self.queryset.filter(author=request.user)
         return super().list(request, *args, *kwargs)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """
+        Вьюсет для модели Отзывов (Comment)
+    """
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
 
+    def perform_create(self, serializer):
+        """
+        Создание комментария и установление автора.
+        """
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(ad=self.kwargs['ad_pk']).select_related("author")
