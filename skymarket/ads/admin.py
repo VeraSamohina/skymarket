c00e4852eedb44c20ab8bd3d60504556201b0e89
samohianvera <samohinavera@gmail.com>
2024-01-03 18:58:37 +0300

    add models Ad and Comment in admin

--- skymarket/ads/admin.py
+++ skymarket/ads/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 
 from ads.models import Ad, Comment
 
-# TODO здесь можно подкючить ваши модели к стандартной джанго-админке
 
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    """
+    Объявления - отображение в админ-панели Django.
+    Фильтрует по названию товара.
+    Поиск в названии товара и его описании.
+    """
+    list_display = ('pk', 'author', 'title', 'price', 'created_at')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Отзывы - отображение в админ-панели Django.
+    Фильтрует по автору.
+    """
+    list_display = ('pk', 'ad', 'author', 'text', 'created_at')
+    list_filter = ('author',)
